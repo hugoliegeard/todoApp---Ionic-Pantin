2b32c57cbc7704dd898bf6d5dc4b6539508c640f
Hugo LIEGEARD <hugo_liegeard@yahoo.fr>
2018-07-25 10:25:35 +0200

    Debut TodoApp

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Task} from "../../models/task";
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,32 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  /** Création d'une tâche @type {Task} */
+  task: Task = new Task();
+
+  /** Liste des tâches @type {Task[]} */
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Promener le chien',
+      status: false
+    },
+    {
+      id: 2,
+      title: 'Arroser les plantes',
+      status: true
+    },
+    {
+      id: 3,
+      title: 'Sortir les poubelles',
+      status: false
+    },
+    {
+      id: 4,
+      title: 'Faire la vaisselle',
+      status: true
+    }
+  ];
 
 }
